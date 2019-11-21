d35e9131cfceadc4ca25f734a7534e7c7fdabb9b
edencoder <alex@edenup.com>
2019-11-22 09:22:26 +1000

    disable data dir

--- bundles/serviceworker/tasks/serviceworker.js
+++ bundles/serviceworker/tasks/serviceworker.js
@@ -1,20 +1,6 @@
 
 // Require dependencies
-const gulp           = require('gulp');
-const glob           = require('@edenjs/glob');
-const babel          = require('@babel/core');
-const xtend          = require('xtend');
-const config         = require('config');
-const babelify       = require('babelify');
-const watchify       = require('watchify');
-const browserify     = require('browserify');
-const gulpTerser     = require('gulp-terser');
-const gulpHeader     = require('gulp-header');
-const vinylBuffer    = require('vinyl-buffer');
-const vinylSource    = require('vinyl-source-stream');
-const browserifyinc  = require('browserify-incremental');
-const gulpSourcemaps = require('gulp-sourcemaps');
-const babelPresetEnv = require('@babel/preset-env');
+const config = require('config');
 
 /**
  * Build serviceworker task class
@@ -37,120 +23,107 @@ class ServiceworkerTask {
     this.watch = this.watch.bind(this);
   }
 
-  async _browserify(files) {
-    if (this._b !== null) {
-      return this._b;
-    }
+  /**
+   * runs serviceworker
+   *
+   * @param {Array} files 
+   */
+  async run(files) {
+    // set opts
+    const opts = {
+      files,
+
+      dest       : `${global.appRoot}/www/public/js`,
+      cache      : `${global.appRoot}/.edenjs/.cache/serviceworker.json`,
+      imports    : global.importLocations,
+      browsers   : config.get('browserlist'),
+      polyfill   : require.resolve('@babel/polyfill'),
+      sourceMaps : config.get('environment') === 'dev' && !config.get('noSourcemaps'),
+
+      appRoot  : global.appRoot,
+      edenRoot : global.edenRoot,
+    };
+
+    // run in thread
+    return this._runner.thread(this.thread, opts);
+  }
+
+  /**
+   * threadded run
+   */
+  async thread(data) {
+    // require
+    const gulp           = require('gulp');
+    const glob           = require('@edenjs/glob');
+    const xtend          = require('xtend');
+    const babel          = require('@babel/core');
+    const babelify       = require('babelify');
+    const browserify     = require('browserify');
+    const gulpTerser     = require('gulp-terser');
+    const vinylSource    = require('vinyl-source-stream');
+    const vinylBuffer    = require('vinyl-buffer');
+    const browserifyinc  = require('browserify-incremental');
+    const gulpSourcemaps = require('gulp-sourcemaps');
+    const babelPresetEnv = require('@babel/preset-env');
 
     // Browserify javascript
     let b = browserify(xtend(browserifyinc.args, {
-      paths         : global.importLocations,
-      debug         : config.get('environment') === 'dev' && !config.get('noSourcemaps'),
-      entries       : [require.resolve('@babel/polyfill'), ...await glob(files)],
+      paths         : data.imports,
+      watch         : true,
+      debug         : data.sourcemaps,
+      entries       : [data.polyfill, ...await glob(data.files)],
       commondir     : false,
       insertGlobals : true,
-      cache         : {},
-      packageCache  : {},
     }));
 
     // browserifyinc
     browserifyinc(b, {
-      cacheFile : `${global.appRoot}/.edenjs/.cache/serviceworker.json`,
+      cacheFile : data.cache,
     });
 
+    // check environment
     b = b.transform(babelify, {
-      sourceMaps : config.get('environment') === 'dev' && !config.get('noSourcemaps'),
-      presets    : [
+      presets : [
         babel.createConfigItem([babelPresetEnv, {
-          useBuiltIns : 'entry',
-          targets     : {
-            browsers : config.get('browserlist'),
+          targets : {
+            browsers : data.browsers,
           },
+          useBuiltIns : 'entry',
         }]),
       ],
+      sourceMaps : data.sourcemaps,
     });
-
-    b.plugin(watchify, {
-      poll        : false,
-      ignoreWatch : ['*'],
-    });
-
-    this._b = b;
-
-    return b;
-  }
-
-  /**
-   * Run assets task
-   *
-   * @param {Array} files
-   *
-   * @return {Promise}
-   */
-  async run(files) {
-    // browserify files
-    const b = await this._browserify(files);
 
     // Create browserify bundle
     const bundle = b.bundle();
 
     // Create job from browserify bundle
     let job = bundle
-      .pipe(vinylSource('sw.js')) // Convert to gulp stream
+      .pipe(vinylSource('app.min.js')) // Convert to gulp stream
       .pipe(vinylBuffer()); // Needed for terser, sourcemaps
 
     // Init gulpSourcemaps
-    if (config.get('environment') === 'dev' && !config.get('noSourcemaps')) {
+    if (data.sourceMaps) {
       job = job.pipe(gulpSourcemaps.init({ loadMaps : true }));
     }
 
-    // get offline routes
-    const routes = cache('routes').filter((route) => {
-      // return route is offline
-      return !!route.offline;
-    }).map((route) => {
-      // return mapped route
-      return {
-        acl      : route.acl,
-        fail     : route.fail,
-        type     : route.type,
-        view     : route.view,
-        route    : route.path,
-        title    : route.title,
-        layout   : route.layout,
-        priority : route.priority,
-      };
-    }).sort((a, bb) => (bb.priority || 0) - (a.priority || 0));
-
-    // Build vendor prepend
-    const head = `
-      self.config = ${JSON.stringify(config.get('serviceworker.config') || {})};
-      ${config.get('serviceworker.config.offline') ? `self.config.routes = ${JSON.stringify(routes)};` : ''}
-    `;
-
-    // Apply head to file
-    job = job.pipe(gulpHeader(head, false));
-
-    // Only minify in live
-    if (config.get('environment') === 'live') {
-      // Pipe uglify
-      job = job.pipe(gulpTerser({
-        ie8    : false,
-        mangle : true,
-        output : {
-          comments : false,
-        },
-        compress : true,
-      }));
-    }
+    // Pipe uglify
+    job = job.pipe(gulpTerser({
+      ie8      : false,
+      mangle   : true,
+      compress : true,
+      output   : {
+        comments : false,
+      },
+    }));
 
     // Write gulpSourcemaps
-    if (config.get('environment') === 'dev' && !config.get('noSourcemaps')) {
+    if (data.sourceMaps) {
       job = job.pipe(gulpSourcemaps.write('.'));
     }
 
     // Pipe job
-    job = job.pipe(gulp.dest(`${global.appRoot}/data/www`));
+    job = job.pipe(gulp.dest(data.dest));
 
     // Wait for job to end
     await new Promise((resolve, reject) => {
